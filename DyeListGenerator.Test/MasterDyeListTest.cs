c1e7f19e888626896e96bce1a2c3fcf949445fe4
Matt Leuer <mattleuer@gmail.com>
2019-09-27 12:15:36 -0500

    ExtractYarnCounts combines all yarn from customer orders.

--- DyeListGenerator.Test/MasterDyeListTest.cs
+++ DyeListGenerator.Test/MasterDyeListTest.cs
@@ -17,15 +17,28 @@ namespace DyeListGenerator.Test
                 }), 
                 new Customer("Eat Sleep Knit", new List<Yarn>()
                 {
-                    new Yarn() {NumberOfSkeins = 10, YarnType = YarnType.Classy, YarnTypeDescription = "Classy S.W. Merino Worsted", Color = "A Little Night Music"},
-                    new Yarn() {NumberOfSkeins = 6, YarnType = YarnType.SmooshyWCashmere, YarnTypeDescription = "Smooshy Cashmere (Mixed Lot)", Color = "Milky Spite"}
+                    new Yarn() {NumberOfSkeins = 20, YarnType = YarnType.Classy, YarnTypeDescription = "Classy S.W. Merino Worsted", Color = "A Little Night Music"},
+                    new Yarn() {NumberOfSkeins = 3, YarnType = YarnType.SmooshyWCashmere, YarnTypeDescription = "Smooshy Cashmere (Mixed Lot)", Color = "Milky Spite"}
+                }),
+                new Customer("Baa Baa Sheep", new List<Yarn>()
+                {
+                    new Yarn() {NumberOfSkeins = 5, YarnType = YarnType.Classy, YarnTypeDescription = "Classy S.W. Merino Worsted", Color = "A Little Night Music"},
+                    new Yarn() {NumberOfSkeins = 10, YarnType = YarnType.Classy, YarnTypeDescription = "Classy Merino Worsted", Color = "A Little Night Music"},
+                    new Yarn() {NumberOfSkeins = 3, YarnType = YarnType.SmooshyWCashmere, YarnTypeDescription = "Smooshy Cashmere (Mixed Lot)", Color = "Milky Spite"}
                 })
             };
-            
-            Dictionary<Yarn, uint> yarnTotals = new Dictionary<Yarn, uint>()
+
+            ISet<Yarn> yarnTotals = new HashSet<Yarn>()
             {
-                
+                new Yarn() {NumberOfSkeins = 35, YarnType = YarnType.Classy, YarnTypeDescription = "Classy S.W. Merino Worsted", Color = "A Little Night Music"},
+                new Yarn() {NumberOfSkeins = 6, YarnType = YarnType.Smooshy, YarnTypeDescription = "Smooshy Sock Superwash Merino", Color = "6 of Everything"},
+                new Yarn() {NumberOfSkeins = 6, YarnType = YarnType.SmooshyWCashmere, YarnTypeDescription = "Smooshy Cashmere (Mixed Lot)", Color = "Milky Spite"},
+                new Yarn() {NumberOfSkeins = 10, YarnType = YarnType.Classy, YarnTypeDescription = "Classy Merino Worsted", Color = "A Little Night Music"},
             };
+
+            ISet<Yarn> actualYarnTotals = MasterDyeList.ExtractYarnCounts(customers);
+            
+            CollectionAssert.AreEquivalent(yarnTotals, actualYarnTotals);
         }
     }
 }
--- DyeListGenerator/MasterDyeList.cs
+++ DyeListGenerator/MasterDyeList.cs
@@ -26,9 +26,64 @@ namespace DyeListGenerator
             
         }
 
-        public static Dictionary<Yarn, uint> ExtractYarnCounts(List<Customer> customers)
+        public static ISet<Yarn> ExtractYarnCounts(List<Customer> customers)
         {
-            return null;
+            HashSet<Yarn> yarnTotal = new HashSet<Yarn>();
+            List<Yarn> combinedYarnOrders = extractListOfYarn(customers);
+            
+            for (var i = 0; i < combinedYarnOrders.Count; i++)
+            {
+                var yarn1 = combinedYarnOrders[i];
+                for (var j = i + 1; j < combinedYarnOrders.Count; j++)
+                { 
+                    var yarn2 = combinedYarnOrders[j];
+
+                    if (!ReferenceEquals(yarn1, yarn2))
+                    {
+                        if (Yarn.AreEquivalent(yarn1, yarn2))
+                        {
+                            yarn1 += yarn2;
+                            combinedYarnOrders.Remove(yarn2);
+                        }
+                    }
+                }
+                yarnTotal.Add(yarn1);
+            }
+
+//            for (var i = 0; i < combinedYarnOrders.Count; i++)
+//            {
+//                var yarn1 = combinedYarnOrders[i];
+//                Yarn combinedYarn = yarn1;
+//
+//                for (var j = i + 1; j < combinedYarnOrders.Count; j++)
+//                {
+//                    var yarn2 = combinedYarnOrders[j];
+//                    if (!Object.ReferenceEquals(yarn1, yarn2))
+//                    {
+//                        if (Yarn.AreEquivalent(yarn1, yarn2))
+//                        {
+//                            combinedYarn += yarn2;
+//                        }
+//                    }
+//                }
+//                yarnTotal.Add(combinedYarn);
+//            }
+//
+            return yarnTotal;
+        }
+
+        private static List<Yarn> extractListOfYarn(List<Customer> customers)
+        {
+            List<Yarn> combinedYarn = new List<Yarn>();
+            foreach (var customer in customers)
+            {
+                foreach (var yarn in customer.Order)
+                {
+                    combinedYarn.Add(yarn);
+                }
+            }
+
+            return combinedYarn;
         }
     }
 }
